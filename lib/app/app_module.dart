87763a4a8bb90c9f38b622af7585b7c6ede2f686
israelhudson <israelhudson@gmail.com>
2020-01-28 00:34:47 -0300

    Video Player iniciado

---
+++ lib/app/app_module.dart
@@ -0,0 +1,24 @@
+import 'package:video_media_player_flutter/app/app_controller.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter/material.dart';
+import 'package:video_media_player_flutter/app/app_widget.dart';
+import 'package:video_media_player_flutter/app/modules/home/home_module.dart';
+import 'package:video_media_player_flutter/app/modules/player/player_module.dart';
+
+class AppModule extends MainModule {
+  @override
+  List<Bind> get binds => [
+        Bind((i) => AppController()),
+      ];
+
+  @override
+  List<Router> get routers => [
+        Router('/', module: HomeModule()),
+        Router('/player', module: PlayerModule()),
+      ];
+
+  @override
+  Widget get bootstrap => AppWidget();
+
+  static Inject get to => Inject<AppModule>.of();
+}
---
+++ lib/app/modules/player/player_page.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+
+class PlayerPage extends StatefulWidget {
+  final String title;
+  const PlayerPage({Key key, this.title = "Player"}) : super(key: key);
+
+  @override
+  _PlayerPageState createState() => _PlayerPageState();
+}
+
+class _PlayerPageState extends State<PlayerPage> {
+
+  VideoPlayerController _controller;
+  Future<void> _initializeVideoPlayerFuture;
+
+  @override
+  void initState() {
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+    );
+
+    _initializeVideoPlayerFuture = _controller.initialize();
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // Ensure disposing of the VideoPlayerController to free up resources.
+    _controller.dispose();
+
+    super.dispose();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            width: 300,
+            height: 200,
+            child: FutureBuilder(
+              future: _initializeVideoPlayerFuture,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  // If the VideoPlayerController has finished initialization, use
+                  // the data it provides to limit the aspect ratio of the VideoPlayer.
+                  return AspectRatio(
+                    aspectRatio: _controller.value.aspectRatio,
+                    // Use the VideoPlayer widget to display the video.
+                    child: VideoPlayer(_controller),
+                  );
+                } else {
+                  // If the VideoPlayerController is still initializing, show a
+                  // loading spinner.
+                  return Center(child: CircularProgressIndicator());
+                }
+              },
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Wrap the play or pause in a call to `setState`. This ensures the
+          // correct icon is shown.
+          setState(() {
+            // If the video is playing, pause it.
+            if (_controller.value.isPlaying) {
+              _controller.pause();
+            } else {
+              // If the video is paused, play it.
+              _controller.play();
+            }
+          });
+        },
+        // Display the correct icon depending on the state of the player.
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
+        ),
+      ),
+    );
+  }
+}
